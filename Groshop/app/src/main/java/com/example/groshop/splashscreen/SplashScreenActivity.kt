3d50fbd617728747af0697aff07ccde1475fd7af
Bansi Mamtora <mbansi306@gmail.com>
2022-05-10 20:11:50 +0530

    connected splash and sign up

--- Groshop/app/src/main/java/com/example/groshop/splashscreen/SplashScreenActivity.kt
+++ Groshop/app/src/main/java/com/example/groshop/splashscreen/SplashScreenActivity.kt
@@ -2,11 +2,11 @@ package com.example.groshop.splashscreen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.groshop.MainActivity
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
 import com.example.groshop.R
+import com.example.groshop.authentication.SignInActivity
 
 class SplashScreenActivity: AppCompatActivity() {
 
@@ -15,7 +15,7 @@ class SplashScreenActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         Handler(Looper.getMainLooper()).postDelayed( {
-            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashScreenActivity, SignInActivity::class.java)
             startActivity(intent)
             finish()
         }, 2500)
@@ -25,3 +25,11 @@ class SplashScreenActivity: AppCompatActivity() {
         super.onDestroy()
     }
 }
+
+
+
+
+
+
+
+
