c8f83ab60bc716a7c64251eb6f57ea4245a49d93
Bansi Mamtora <mbansi306@gmail.com>
2022-05-11 11:22:37 +0530

    onboard screen

---
+++ Groshop/app/src/main/java/com/example/groshop/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,48 @@
+package com.example.groshop.onboarding
+
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.groshop.R
+import com.example.groshop.databinding.ActivityOnboardingBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        supportActionBar?.hide()
+        binding.vpOnBoarding.adapter = OnBoardingAdapter(OnBoardingModel.getOnBoardData())
+        TabLayoutMediator(binding.tab, binding.vpOnBoarding) { tab, position -> }.attach()
+        setSpannableText()
+    }
+
+    private fun setSpannableText() {
+        val spannable = SpannableString(binding.tvAlready.text)
+        val clickableSpan2: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(p0: View) {
+                Toast.makeText(applicationContext, "This", Toast.LENGTH_LONG).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                ds.setColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+                ds.bgColor = ContextCompat.getColor(applicationContext, R.color.white)
+            }
+        }
+        spannable.setSpan(clickableSpan2, 25, 32, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvAlready.text = spannable
+        binding.tvAlready.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
